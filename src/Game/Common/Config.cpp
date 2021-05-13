a70cd8d4ddf3ae3c5653c9d432fb16a99f2de58e
Thunraz <thunraz@thunraz.com>
2021-05-13 11:27:15 +0200

    Use json::parse instead of deprecated method

--- src/Game/Common/Config.cpp
+++ src/Game/Common/Config.cpp
@@ -6,13 +6,11 @@
 ConfigSettings::ConfigSettings(std::string input_file_path)
 {
     std::ifstream file_stream("config.json", std::ios_base::in);
-
     if (file_stream.fail()) {
         throw std::exception { "could not open config.json" };
     }
 
-    nlohmann::json json;
-    json << file_stream;
+    nlohmann::json json = nlohmann::json::parse(file_stream);
 
     json.at("serverIp").get_to(serverIp);
     json.at("serverPort").get_to(serverPort);
