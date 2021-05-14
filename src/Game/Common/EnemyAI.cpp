449edcc66a0c7af4254509c495f33fbb967ed287
Simon Weis <laguna.ws@gmail.com>
2021-05-14 10:10:55 +0200

    Add Shots to EnemyAI

---
+++ src/Game/Common/EnemyAI.cpp
@@ -0,0 +1,39 @@
+﻿#include "EnemyAI.hpp"
+#include "EnemyState.hpp"
+
+void EnemyAI::move(EnemyState& state, std::vector<ShotState>& shots, float elapsed)
+{
+    jt::Vector2 const offset { 0, Game::GameProperties::scrollSpeed() * elapsed };
+    state.position += offset;
+    state._positionBase += offset;
+    doMove(state, shots, elapsed);
+}
+
+void EnemyAIIdle::doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed) { }
+
+void EnemyAISine::doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed)
+{
+    const float posOffsetX
+        = static_cast<float>((sin(state._age * 2.0f + state._moveDelay) * 25.0f));
+    state.position = state._positionBase + jt::Vector2 { posOffsetX, 0.0f };
+}
+
+void EnemyAICircle::doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed)
+{
+    if (m_sequenceId == 0) {
+        if (state.position.y() >= 100) {
+            m_sequenceId = 1;
+            m_timer = 0.0f;
+        }
+    } else if (m_sequenceId == 1) {
+        m_timer += elapsed;
+        // remove normal movement
+        jt::Vector2 const normalMoveOffset { 0, Game::GameProperties::scrollSpeed() * elapsed };
+        state.position -= normalMoveOffset;
+        state._positionBase -= normalMoveOffset;
+
+        const float posOffsetX = static_cast<float>((sin(m_timer * 1.85f) * 48.0f));
+        const float posOffsetY = static_cast<float>(((cos(m_timer * 1.85f) - 1) * 48.0f));
+        state.position = state._positionBase + jt::Vector2 { posOffsetX, posOffsetY };
+    }
+}
---
+++ src/Game/Common/EnemyAI.hpp
@@ -0,0 +1,28 @@
+﻿#ifndef ENEMY_AI_HPP_GUARD
+#define ENEMY_AI_HPP_GUARD
+#include "EnemyState.hpp"
+#include "ShotState.hpp"
+class EnemyAI {
+public:
+    EnemyAI() = default;
+    ~EnemyAI() = default;
+    void move(EnemyState& state, std::vector<ShotState>& shots, float elapsed);
+
+private:
+    virtual void doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed) = 0;
+};
+
+class EnemyAIIdle : public EnemyAI {
+    void doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed) override;
+};
+
+class EnemyAISine : public EnemyAI {
+    void doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed) override;
+};
+
+class EnemyAICircle : public EnemyAI {
+    void doMove(EnemyState& state, std::vector<ShotState>& shots, float elapsed) override;
+    int m_sequenceId { 0 };
+    float m_timer { 0.0f };
+};
+#endif
